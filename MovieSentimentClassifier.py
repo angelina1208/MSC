92c658bf61c5f133e681fc2027ebe9929f02c84f
Angelina-Sophia Hauswald <ahauswald@uni-potsdam.de>
2020-08-22 14:41:38 +0200

    Stub hinzugefuegt der daran erinnern soll, das man einmal zum nutzen des Programms resourcen herunterzuladen sind. Spaeter fuer install script vielleicht?

--- MovieSentimentClassifier.py
+++ MovieSentimentClassifier.py
@@ -1,9 +1,18 @@
 import os
+import nltk
 
-# do your stuff
+
+def ask_for_downloads():
+	print("Do you need to download stuff?")
+	nltk.download('punkt')
+	nltk.download('averaged_perceptron_tagger')
+	nltk.download('wordnet')
+	print("You downloaded stuff.")
+
 
 if __name__ == '__main__':
 	print("Welcome to MSC.")
+	ask_for_downloads()
 	pos_folder = "aclImdb_v1/aclImdb/train/pos"
 	neg_folder = "aclImdb_v1/aclImdb/train/neg"
 	print("Training with positive samples from: {}\nand negative samples from: {}".format(pos_folder, neg_folder))
