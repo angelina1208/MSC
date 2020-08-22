6f28e08d4bfa75b6ee89bb2859848ff71a1e4c59
Angelina-Sophia Hauswald <ahauswald@uni-potsdam.de>
2020-08-22 15:05:15 +0200

    Schreibt features in csv, es fehlen noch ordner der datei, goldlabel, vl eine headerzeile fuer die csv...

--- MovieSentimentClassifier.py
+++ MovieSentimentClassifier.py
@@ -1,4 +1,5 @@
 import os
+
 import nltk
 
 from MSC_features import regex_countable_features, nltk_countable_features
@@ -17,14 +18,24 @@ if __name__ == '__main__':
 	ask_for_downloads()
 	pos_folder = "aclImdb_v1/aclImdb/train/pos"
 	neg_folder = "aclImdb_v1/aclImdb/train/neg"
+	feature_store_file = "feats.csv"
 	print("Training with positive samples from: {}\nand negative samples from: {}".format(pos_folder, neg_folder))
 	for filename in os.listdir(pos_folder):
-		with open(os.path.join(os.getcwd(), pos_folder,  filename), 'r') as f:
+		with open(os.path.join(os.getcwd(), pos_folder, filename), 'r') as f:
 			text = f.read()
 
-		#print(text)
+		# print(text)
 		all_features = {}
 		all_features.update(regex_countable_features(text))
 		all_features.update(nltk_countable_features(text))
+		line = filename + ","
+		keys_sorted = sorted(all_features.keys())
+		for key in keys_sorted:
+			line = line + str(all_features[key]) + ","
+		line = line[0:-1] + "\n"
+		with open(feature_store_file, "a") as s:
+			s.write(line)
 
-		#print(all_features)
+
+
+# print(all_features)
